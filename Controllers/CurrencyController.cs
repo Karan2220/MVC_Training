a91509a24a79052bc140129f3c977013e5ab23a5
Karan2220 <kanaparthisrija@gmail.com>
2019-08-19 16:30:37 +0530

    mvc training

---
+++ Controllers/CurrencyController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        public ActionResult Index(int amount)
+        {
+            int[] currencyNotes = new int[] { 1000, 500, 100, 50, 10, 5, 1 };
+            int[] counter = new int[10];
+
+           for(int i=0;i<=6;i++)
+            {                
+                counter[i] = amount / currencyNotes[i];
+                amount = amount - counter[i] * currencyNotes[i];
+            }
+           
+            for (int i = 0; i <=9; i++)
+            {
+                if(counter[i]!=0)
+                {
+                    ViewBag.counter = counter[i];
+                    ViewBag.actualValue = currencyNotes[i];
+                }
+            }
+
+            ViewBag.text = "";
+            int value = 0;
+            switch(value)
+            { 
+                case 1000: ViewBag.text = "Thousand rupees notes";
+                    break;
+                case 500:
+                    ViewBag.text = "Five hundred rupees notes";
+                    break;
+                case 100:
+                    ViewBag.text = "Hundred rupees notes";
+                    break;
+                case 50:
+                    ViewBag.text = "Fifty rupees notes";
+                    break;
+                case 10:
+                    ViewBag.text = "Ten rupees notes";
+                    break;
+                case 5:
+                    ViewBag.text = "Five rupees notes";
+                    break;
+                case 1:
+                    ViewBag.text = "One rupee notes";
+                    break;
+                default:
+                    break;
+            }
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string str)
+        {
+            string filepath = "";
+
+            if (str == "sample.pdf")
+            {               
+                filepath = "~/Files/sample.pdf";
+                return File(filepath, "application/pdf");        
+            }
+            else if (str == "gotoabout")
+            {
+                return RedirectToAction("About", "Home");
+            }
+            else if(str == "login")
+            {
+                return View("Index");
+            }
+            else if(str!= null)
+            {
+                return Content("You entered:" + str);
+            }
+            else
+            {
+                return Content("");
+            }       
+        }
+
+        public ActionResult About()
+        {
+            return Content("About content here");
+        }
+
+    }
+}
